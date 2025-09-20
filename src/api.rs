ee360cf5375bf0fb343f157ff2107ef3e3ce7be4
Nuwan Rajika Kumarasiri <nuwan.r.kumarasiri@gmail.com>
2025-09-20 09:09:34 -0500

    rust network api

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,8 @@
-// #![allow(dead_code)]
-
+use std::{io, thread};
 use std::io::{BufRead, BufReader, Error, Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::{io, thread};
 
-fn main() {
+fn tcp_server() {
     let listener = TcpListener::bind("127.0.0.1:32100").expect("could not bind");
     for stream in listener.incoming() {
         match stream {
@@ -33,3 +31,16 @@ fn handle_conn(mut stream: TcpStream) -> Result<(), Error> {
     }
 }
 
+fn tcp_client() {
+    let mut stream = TcpStream::connect("127.0.0.1:32100").expect("could not connect to the server");
+    loop {
+        let mut input = String::new();
+        let mut buffer: Vec<u8> = Vec::new();
+        io::stdin().read_line(&mut input).expect("failed to read from stdin");
+        stream.write(input.as_bytes()).expect("failed to write to server");
+
+        let mut read = BufReader::new(&stream);
+        read.read_until(b'\n', &mut buffer).expect("could not read into the buffer");
+        println!("{}", str::from_utf8(&buffer).expect("could not write buffer as string"));
+    }
+}
